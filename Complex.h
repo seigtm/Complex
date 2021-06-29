e443eddc690c7d10fee09b324a4654d855cae5bf
Костыль <seigtm@gmail.com>
2021-06-29 15:13:24 +0300

    Minor includes fixes

--- Complex.h
+++ Complex.h
@@ -1,40 +1,42 @@
 #pragma once
 #include <iostream>
 #include <cmath>
+#include <limits>
 
-class Complex {
+class Complex
+{
 
 public:
   Complex();
   Complex(double r, double i = 0);
 
   // Addition operators.
-  Complex& operator += (const Complex& other);
-  Complex operator + (const Complex& other);
-  Complex operator + (double value);
-  friend Complex operator + (double value, Complex mc);
+  Complex &operator+=(const Complex &other);
+  Complex operator+(const Complex &other);
+  Complex operator+(double value);
+  friend Complex operator+(double value, Complex mc);
 
   // Unary operators.
-  Complex operator - () const;
-  Complex operator + () const;
+  Complex operator-() const;
+  Complex operator+() const;
 
   // Substraction operators.
-  Complex& operator -= (const Complex& other);
-  Complex operator - (const Complex& other);
-  Complex operator - (double value);
-  friend Complex operator - (double value, Complex mc);
+  Complex &operator-=(const Complex &other);
+  Complex operator-(const Complex &other);
+  Complex operator-(double value);
+  friend Complex operator-(double value, Complex mc);
 
   // Multiplication operators.
-  Complex& operator *= (const Complex& other);
-  Complex operator * (const Complex& other);
-  Complex operator * (double value);
-  friend Complex operator * (double value, Complex mc);
+  Complex &operator*=(const Complex &other);
+  Complex operator*(const Complex &other);
+  Complex operator*(double value);
+  friend Complex operator*(double value, Complex mc);
 
   // Division operators.
-  Complex& operator /= (const Complex& other);
-  Complex operator / (const Complex& other);
-  Complex operator / (double value);
-  friend Complex operator / (double value, Complex mc);
+  Complex &operator/=(const Complex &other);
+  Complex operator/(const Complex &other);
+  Complex operator/(double value);
+  friend Complex operator/(double value, Complex mc);
 
   // Exponentiation using de Moivre's formula.
   friend Complex pow(Complex complex, double power);
@@ -43,77 +45,83 @@ public:
   friend Complex nrt(Complex complex, double power);
 
   // Trigonometric functions.
-  friend Complex sin(const Complex& complex);
-  friend Complex cos(const Complex& complex);
+  friend Complex sin(const Complex &complex);
+  friend Complex cos(const Complex &complex);
 
   // Increment operators.
-  Complex& operator ++ ();   // x++.
-  Complex operator ++ (int); // ++x.
+  Complex &operator++();   // x++.
+  Complex operator++(int); // ++x.
 
   // Decrement operators.
-  Complex& operator -- ();   // x--.
-  Complex operator -- (int); // --x.
+  Complex &operator--();   // x--.
+  Complex operator--(int); // --x.
 
   // Comparison operators.
-  bool operator ==(const Complex& other) const;
-  bool operator !=(const Complex& other) const;
+  bool operator==(const Complex &other) const;
+  bool operator!=(const Complex &other) const;
 
-  template<class T>
-  bool operator ==(const T& other) const {
+  template <class T>
+  bool operator==(const T &other) const
+  {
     if (!isEqual(img, 0.0))
       return false;
 
     return isEqual(real, other);
   }
 
-  template<class T>
-  bool operator !=(const T& other) const {
+  template <class T>
+  bool operator!=(const T &other) const
+  {
     if (isEqual(img, 0.0))
       return !isEqual(real, other);
 
     return true;
   }
 
-  template<class T>
-  friend bool operator ==(const T& other, const Complex& complex) {
+  template <class T>
+  friend bool operator==(const T &other, const Complex &complex)
+  {
     return complex == other;
   }
 
-  template<class T>
-  friend bool operator !=(const T& other, const Complex& complex) {
+  template <class T>
+  friend bool operator!=(const T &other, const Complex &complex)
+  {
     return complex != other;
   }
 
   // Bitwise operators.
-  friend std::ostream& operator <<(std::ostream& out, const Complex& complex);
-  friend std::istream& operator >>(std::istream& in, Complex& complex);
-
+  friend std::ostream &operator<<(std::ostream &out, const Complex &complex);
+  friend std::istream &operator>>(std::istream &in, Complex &complex);
 
 private:
   double real;
   double img;
 
+  /*
   template<class T>
   bool isEqual(const double& a, const T& b) const {
     return a == b;
   }
+  */
 
-  bool isEqual(const double& a, const double& b) const {
+  bool isEqual(const double &a, const double &b) const
+  {
     return std::fabs(a - b) < std::numeric_limits<double>::epsilon();
   }
 };
 
 // Friend functions:
-Complex operator + (double value, Complex mc);
-Complex operator - (double value, Complex mc);
-Complex operator * (double value, Complex mc);
-Complex operator / (double value, Complex mc);
+Complex operator+(double value, Complex mc);
+Complex operator-(double value, Complex mc);
+Complex operator*(double value, Complex mc);
+Complex operator/(double value, Complex mc);
 
-Complex sin(const Complex& complex);
-Complex cos(const Complex& complex);
+Complex sin(const Complex &complex);
+Complex cos(const Complex &complex);
 
 Complex pow(Complex complex, const double power); // Exponentiation.
 Complex nrt(Complex complex, const double power); // nth root.
 
-std::ostream& operator <<(std::ostream& out, const Complex& complex);
-std::istream& operator >>(std::istream& in, Complex& complex);
+std::ostream &operator<<(std::ostream &out, const Complex &complex);
+std::istream &operator>>(std::istream &in, Complex &complex);
--- main.cpp
+++ main.cpp
@@ -1,36 +1,35 @@
-﻿#include "Complex.h"
-// const double PI = 3.141592653589793238463;
+﻿#include "Complex.cpp"
+#include <iostream>
 
-void printTwoComplex(const Complex& first, const Complex& second) {
+void printTwoComplex(const Complex &first, const Complex &second)
+{
   std::cout << "First = " << first << std::endl
-            << "Second = " << second << std::endl << std::endl;
+            << "Second = " << second << std::endl
+            << std::endl;
 }
 
-
-int main() {
+int main()
+{
   std::cout << "~~~ COMPLEX NUMBERS ~~~\n\n";
-
-  /*
-        Complex first(1.1, 2.2);
-        Complex second(-5.0, -12.3);
-    */
 
   // Bitwise
   std::cout << "Enter the first complex number = ";
   Complex first;
   std::cin >> first;
-  std::cout << "First = " << first << std::endl << std::endl;
+  std::cout << "First = " << first << std::endl
+            << std::endl;
 
   std::cout << "Enter the second complex number = ";
   Complex second;
   std::cin >> second;
-  std::cout << "Second = " << second << std::endl << std::endl;
+  std::cout << "Second = " << second << std::endl
+            << std::endl;
 
   printTwoComplex(first, second);
 
-
   // Addition
-  std::cout << "~~~~~~~~~~~~~~\n" << "Addition" << std::endl;
+  std::cout << "~~~~~~~~~~~~~~\n"
+            << "Addition" << std::endl;
   std::cout << "first + second = \t" << first + second << std::endl;
   std::cout << "first + -12.9 = \t" << first + -12.9 << std::endl;
   std::cout << "14.2 + second = \t" << 14.2 + second << std::endl;
@@ -38,7 +37,8 @@ int main() {
 
   // Substraction
   printTwoComplex(first, second);
-  std::cout << "~~~~~~~~~~~~~~\n" << "Substraction" << std::endl;
+  std::cout << "~~~~~~~~~~~~~~\n"
+            << "Substraction" << std::endl;
   std::cout << "first - second = \t" << first - second << std::endl;
   std::cout << "first - -12.9 = \t" << first - -12.9 << std::endl;
   std::cout << "14.2 - second = \t" << 14.2 - second << std::endl;
@@ -46,7 +46,8 @@ int main() {
 
   // Multiplication
   printTwoComplex(first, second);
-  std::cout << "~~~~~~~~~~~~~~\n" << "Multiplication" << std::endl;
+  std::cout << "~~~~~~~~~~~~~~\n"
+            << "Multiplication" << std::endl;
   std::cout << "first * second = \t" << first * second << std::endl;
   std::cout << "first * -12.9 = \t" << first * -12.9 << std::endl;
   std::cout << "14.2 * second = \t" << 14.2 * second << std::endl;
@@ -54,7 +55,8 @@ int main() {
 
   // Division
   printTwoComplex(first, second);
-  std::cout << "~~~~~~~~~~~~~~\n" << "Division" << std::endl;
+  std::cout << "~~~~~~~~~~~~~~\n"
+            << "Division" << std::endl;
   std::cout << "first / second = \t" << first / second << std::endl;
   std::cout << "first / -12.9 = \t" << first / -12.9 << std::endl;
   std::cout << "14.2 / second = \t" << 14.2 / second << std::endl;
@@ -62,7 +64,8 @@ int main() {
 
   // Comparison (equality)
   printTwoComplex(first, second);
-  std::cout << "~~~~~~~~~~~~~~\n" << "Comparison (equality)" << std::endl;
+  std::cout << "~~~~~~~~~~~~~~\n"
+            << "Comparison (equality)" << std::endl;
   std::cout << "first == second = \t" << (first == second) << std::endl;
   std::cout << "first != 5.2 = \t" << (first != 5.2) << std::endl;
   std::cout << "5 == second = \t" << (5 == second) << std::endl;
@@ -70,7 +73,8 @@ int main() {
 
   // Unary
   printTwoComplex(first, second);
-  std::cout << "~~~~~~~~~~~~~~\n" << "Unary" << std::endl;
+  std::cout << "~~~~~~~~~~~~~~\n"
+            << "Unary" << std::endl;
   std::cout << "+first = \t" << +first << std::endl;
   std::cout << "-second = \t" << -second << std::endl;
   std::cout << "-first = \t" << -first << std::endl;
@@ -78,25 +82,29 @@ int main() {
 
   // Exponentiation using de Moivre's formula
   printTwoComplex(first, second);
-  std::cout << "~~~~~~~~~~~~~~\n" << "Exponentiation using de Moivre's formula" << std::endl;
+  std::cout << "~~~~~~~~~~~~~~\n"
+            << "Exponentiation using de Moivre's formula" << std::endl;
   std::cout << "pow(first, 3.0) = \t" << pow(first, 3.0) << std::endl;
   std::cout << "pow(second, -12) = \t" << pow(second, -12) << "\n~~~~~~~~~~~~~~\n\n"; // Not sure about it.
 
   // nth root using power
   printTwoComplex(first, second);
-  std::cout << "~~~~~~~~~~~~~~\n" << "nth root using power" << std::endl;
+  std::cout << "~~~~~~~~~~~~~~\n"
+            << "nth root using power" << std::endl;
   std::cout << "nrt(first, 3.0) = \t" << nrt(first, 3.0) << std::endl;
   std::cout << "nrt(second, -12) = \t" << nrt(second, -12) << "\n~~~~~~~~~~~~~~\n\n";
 
   // Trigonometric functions
   printTwoComplex(first, second);
-  std::cout << "~~~~~~~~~~~~~~\n" << "Trigonometric functions" << std::endl;
+  std::cout << "~~~~~~~~~~~~~~\n"
+            << "Trigonometric functions" << std::endl;
   std::cout << "sin(first) = \t" << sin(first) << std::endl;
   std::cout << "cos(second) = \t" << cos(second) << "\n~~~~~~~~~~~~~~\n\n";
 
   // Increment and decrement
   printTwoComplex(first, second);
-  std::cout << "~~~~~~~~~~~~~~\n" << "Increment and decrement" << std::endl;
+  std::cout << "~~~~~~~~~~~~~~\n"
+            << "Increment and decrement" << std::endl;
   std::cout << "first++ && second--:" << std::endl;
   first++;
   second++;
@@ -107,7 +115,8 @@ int main() {
 
   // Assignment
   printTwoComplex(first, second);
-  std::cout << "~~~~~~~~~~~~~~\n" << "Assigment" << std::endl;
+  std::cout << "~~~~~~~~~~~~~~\n"
+            << "Assigment" << std::endl;
   first = second;
   std::cout << "first = second\n";
   printTwoComplex(first, second);
