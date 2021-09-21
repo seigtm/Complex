668b8f59d6b92f857ee47fb349f0b415118d0f08
seigtm <seigtm@gmail.com>
2021-09-21 12:17:17 +0300

    Version 1.3.0 release

--- conanfile.py
+++ conanfile.py
@@ -1,14 +1,38 @@
-from conans import ConanFile
+from conans import ConanFile, CMake
 
 
 class Complex(ConanFile):
     name = "Complex"
-    version = "1.1.0"
+    version = "1.3.0"
+
+    # Metadata.
     author = "Baranov Konstantin (seigtm@gmail.com)"
     url = "https://github.com/seigtm/Complex"
     description = "A simple complex numbers class that supports all basic operations"
-    settings = "os", "compiler", "build_type", "arch"
-    generators = "cmake", "visual_studio"
 
+    # Binary configuration
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "cmake"
+
+    # Packages requirements.
     build_requires = \
         "gtest/[1.11]"
+
+    # Version control specification.
+    scm = {
+        "type": "git",
+        "subfolder": name,
+        "url": "auto",
+        "revision": "auto",
+        "username": "git"
+    }
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure(source_folder = self.name)
+        cmake.build()
+
+    def package(self):
+        cmake = CMake(self)
+        cmake.configure(source_folder = self.name)
+        cmake.install()
